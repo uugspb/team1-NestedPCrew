d929d0e54a888ea99baa02f1a3a56999e274afe3
Daniil Basmanov <basmanovdaniil@gmail.com>
2016-04-02 22:45:05 +0300

    Created cone

---
+++ Assets/Scripts/Cone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Hackathon
+{
+    public class Cone : MonoBehaviour
+    {
+        public void Place(Vector3 start, Vector3 end, float size)
+        {
+            transform.position = start;
+            Vector3 toEnd = end - start;
+            float z = toEnd.magnitude;
+            transform.localScale = new Vector3(size, size, z);
+            transform.rotation = Quaternion.LookRotation(-toEnd);
+        }
+    }
+}
