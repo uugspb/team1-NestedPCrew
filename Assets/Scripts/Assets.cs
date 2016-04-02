974ffe5b9f0c09e78f1ad31056d3a215efbdde9e
Daniil Basmanov <basmanovdaniil@gmail.com>
2016-04-02 14:09:57 +0300

    Added asset loader

---
+++ Assets/Scripts/Assets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Hackathon
+{
+    public static class Assets
+    {
+        private static readonly string rootNamespace = typeof (Assets).Namespace;
+        private static readonly string rootNamespaceWithDot = rootNamespace + ".";
+
+        public static TObject Load<TObject, TEnum>(TEnum name)
+            where TObject : Object
+            where TEnum : struct, IComparable, IFormattable, IConvertible
+        {
+            return Resources.Load<TObject>(GetPath(name));
+        }
+
+        public static T Load<T>() where T : Object
+        {
+            string fullName = GetCleanFullName<T>();
+            return Resources.Load<T>(fullName);
+        }
+
+        private static string GetPath<TEnum>(TEnum name)
+            where TEnum : struct, IComparable, IFormattable, IConvertible
+        {
+            return GetCleanFullName<TEnum>() + "/" + name;
+        }
+
+        private static string GetCleanFullName<T>()
+        {
+            var type = typeof (T);
+            return type.FullName.Replace(rootNamespaceWithDot, "").Replace(".", "/");
+        }
+    }
+}
