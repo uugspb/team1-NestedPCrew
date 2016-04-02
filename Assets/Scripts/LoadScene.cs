5d6c197b2d5ee6484fd542251d6a4a14a0375c95
Savelev Yuriy <vtornik23@gmail.com>
2016-04-02 17:50:15 +0300

    starting scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	public string sceneName = "Main";
+
+	public void Load () {
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void Load (string _name) {
+		sceneName = _name;
+		Load ();
+	}
+	
+
+}
