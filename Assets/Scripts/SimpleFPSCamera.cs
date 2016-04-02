30f9216c10afd78945b09aa1bc4fafb335687dd5
Sergey Kormishin <s.coremission@gmail.com>
2016-04-02 18:05:31 +0400

    oculus assets, fps controller, etc

---
+++ Assets/Scripts/SimpleFPSCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleFPSCamera : MonoBehaviour {
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    float rotationY = 0F;
+
+    void Update()
+    {
+        if (Application.isEditor)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+    }
+}
