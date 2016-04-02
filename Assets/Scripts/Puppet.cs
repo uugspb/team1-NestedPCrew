cc55f4025b836828ddc568b41d2a89d22b0d2f79
Sergey Kormishin <s.coremission@gmail.com>
2016-04-03 03:42:56 +0400

    stub

--- Assets/Scripts/Puppet.cs
+++ Assets/Scripts/Puppet.cs
@@ -4,6 +4,9 @@ namespace Hackathon
 {
     public class Puppet : TimeBehaviour
     {
+        [SerializeField]
+        private Transform[] wayPoints;
+
         private new Animation animation;
 
         protected virtual void Awake()
@@ -16,6 +19,25 @@ namespace Hackathon
             timeline.Do(true,
                 forward: () => { animation.Play(); },
                 backward: () => { });
+        }
+
+        public void Position(float normalizedPos)
+        {
+            if(wayPoints.Length == 0)
+                return;
+            Transform secondClosestPoint = wayPoints[0];
+            Transform closestPoint = wayPoints[0];
+
+            for (int i = 1; i < wayPoints.Length; i++)
+            {
+                if ((wayPoints[i].position - transform.position).magnitude < (closestPoint.position - transform.position).magnitude)
+                {
+                    secondClosestPoint = closestPoint;
+                    closestPoint = wayPoints[i];
+                }
+            }
+
+            
         }
     }
 }
