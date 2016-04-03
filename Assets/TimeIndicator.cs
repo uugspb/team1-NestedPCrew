15d781365a028a1873e78526496f69effe41f435
Sergey Kormishin <s.coremission@gmail.com>
2016-04-03 09:36:29 +0400

    puppeeter max val

--- Assets/TimeIndicator.cs
+++ Assets/TimeIndicator.cs
@@ -8,6 +8,12 @@ public class TimeIndicator : TimeBehaviour
     [SerializeField]
     private Slider slider;	
 	// Update is called once per frame
+
+    void Start()
+    {
+        var puppeteer = FindObjectOfType<Puppeteer>();
+        slider.maxValue = puppeteer.lifetime;
+    }
 	void Update () {
         slider.value = timeline.time;
 	}
