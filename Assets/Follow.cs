8ad34226840ba7798af24f198c79a0c601d6eefe
Sergey Kormishin <s.coremission@gmail.com>
2016-04-03 06:39:13 +0400

    sky map

---
+++ Assets/Follow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        Vector3 cameraPosition = Camera.main.transform.position;
+        this.transform.position = new Vector3(cameraPosition.x, this.transform.position.y, cameraPosition.z);
+	}
+}
