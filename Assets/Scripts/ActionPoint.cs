3d4d7ccce07cea19ea96f4b2851e90b37596e5ad
Sergey Kormishin <s.coremission@gmail.com>
2016-04-03 10:55:11 +0400

    markers fix

--- Assets/Scripts/ActionPoint.cs
+++ Assets/Scripts/ActionPoint.cs
@@ -31,7 +31,7 @@ public class ActionPoint : MonoBehaviour {
 	public void PrepareClick()
 	{
         canvas.transform.LookAt(Camera.main.transform.position);
-        canvas.transform.RotateAround(Vector3.up, Mathf.PI);
+        //canvas.transform.RotateAround(Vector3.up, Mathf.PI);
 		progressUI.fillAmount = 0;
 		doingTweener = DOTween.To(()=> progressUI.fillAmount, x=> progressUI.fillAmount = x, 1, duration).SetEase(easing).OnComplete(Completed);
 	}
