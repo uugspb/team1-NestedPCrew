5abf9f2525e12d056582c082a914c6c20410f9d7
Sergey Kormishin <s.coremission@gmail.com>
2016-04-03 09:33:18 +0400

    time indicator

---
+++ Assets/TimeIndicator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Hackathon;
+
+public class TimeIndicator : TimeBehaviour
+{
+    [SerializeField]
+    private Slider slider;	
+	// Update is called once per frame
+	void Update () {
+        slider.value = timeline.time;
+	}
+}
